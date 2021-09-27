dae5072ba91034a84336b957c76a3aaa4be4fda5
besimu <76421201+besimu@users.noreply.github.com>
2021-09-27 20:53:50 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 7
+void setup() {
+  pinMode(7,OUTPUT);
+  digitalWrite(7, LOW);
+  delay(1000);
+
+}
+
+void loop() {
+  for (int i = 0; i < 5; i++)
+  {
+    digitalWrite(7, HIGH);
+    delay(100);
+    digitalWrite(7, LOW);
+    delay(100);
+  }
+    while(true)
+    {
+      digitalWrite(7, HIGH);
+      
+    }
+
+
+    
+  }
